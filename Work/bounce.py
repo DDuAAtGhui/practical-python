320187aaae59deb98051e0cc1e68a7de8648d3b6
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2025-10-08 01:58:05 +0900

    practice complete

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,4 +2,12 @@
 #
 # Exercise 1.5
 
-# commit test
+init_height = 100
+height = 0
+
+for i in range(0, 11):
+    if i == 0:
+        height = init_height
+
+    print(f"{i} : {height}")
+    height = round(height * (3 / 5), 4)
