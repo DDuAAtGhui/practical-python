496f85e882eb8ee91a4b5bf2686bcbf8f55e590f
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2025-10-08 20:25:59 +0900

    complete

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,13 +3,30 @@
 # Exercise 1.7
 # mortgage.py
 
-principal = 500000.0
-rate = 0.05
-payment = 2684.11
-total_paid = 0.0
 
-while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
-    total_paid = total_paid + payment
+def totalPaid(principal, rate, payment, exPayStartMon=0, exPayEndMon=0, exPay=0):
+    total_paid = 0
+    elap_month = 0
+    while principal > 0:
+        elap_month += 1
 
-print("Total paid", total_paid)
+        principal = principal * (1 + rate / 12) - payment
+        total_paid += payment
+
+        if elap_month >= exPayStartMon and elap_month <= exPayEndMon:
+            principal -= exPay
+            total_paid += exPay
+
+        print(f"{elap_month} {total_paid} {principal}")
+
+        if principal < payment:
+            total_paid += principal
+            principal = 0
+            elap_month += 1
+            print(f"{elap_month} {total_paid} {principal}")
+            break
+
+    print(f"Elapsed Month : {elap_month}, TotalPaid : {total_paid}")
+
+
+totalPaid(500000.0, 0.05, 2684.11, 61, 108, 1000)
