8aa1ae81af917d9a7fbcd552d62197d4214764ca
mobase <fmofmo1@gmail.com>
2025-10-31 09:17:17 +0900

    make function from pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,20 +3,18 @@
 # Exercise 1.27
 import os
 
-os.chdir("c:/Users/MAINUSER/Desktop/교육/Python/practical-python/Work")
+# os.chdir("c:/Users/MAINUSER/Desktop/교육/Python/practical-python/Work")
 
-pf = "Data/portfolio.csv"
+def portfolio_cost(dir,filename):
+    total_price = 0
+    with open(f"{dir+filename}", "rt") as f:
+        header = next(f)
+        for line in f:
+            row = line.split(",")
+            sum_price = int(row[1]) * float(row[2])
+            total_price += sum_price
+    return total_price
 
-total_price = 0
+print(f"Total cost {portfolio_cost("C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/",
+                                   'portfolio.csv')}")
 
-with open(pf, "rt") as f:
-    print(f.read())
-
-with open(pf, "rt") as f:
-    header = next(f)
-    for line in f:
-        row = line.split(",")
-        sum_price = int(row[1]) * float(row[2])
-        total_price += sum_price
-
-print(f"Total cose {total_price}")
