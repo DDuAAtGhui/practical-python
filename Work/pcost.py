b471da2d7aef88f342ce150fcad52b659bcff13a
mobase <fmofmo1@gmail.com>
2025-10-31 09:42:24 +0900

    change pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
-import os
+import sys
 import csv
 
-# os.chdir("c:/Users/MAINUSER/Desktop/교육/Python/practical-python/Work")
-
-def portfolio_cost(dir,filename):
+manualFilename = "C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/portfolio.csv"
+def portfolio_cost(filename):
     total_price = 0
-    with open(f"{dir+filename}", "rt") as f:
+    with open(f"{filename}", "rt") as f:
         rows = csv.reader(f)
         header = next(rows)
         for row in rows:
@@ -16,6 +15,15 @@ def portfolio_cost(dir,filename):
             total_price += sum_price
     return total_price
 
-print(f"Total cost {portfolio_cost("C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/",
-                                   'portfolio.csv')}")
+print(sys.argv)
 
+for index, a in enumerate(sys.argv):
+    print(f"argv{index} :: ", a)
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+
+else:
+    filename = manualFilename
+
+print("Total Cost :::", portfolio_cost(filename))
