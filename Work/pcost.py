79367809dfe8d5f9cac0021e08145e48b7e6059c
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2025-11-10 23:50:26 +0900

    modify report.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,29 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
-import sys
 import csv
 
-manualFilename = "C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/portfolio.csv"
+# manualFilename = "C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/portfolio.csv"
+
+
 def portfolio_cost(filename):
     total_price = 0
     with open(f"{filename}", "rt") as f:
         rows = csv.reader(f)
-        header = next(rows)
+
         for row in rows:
             sum_price = int(row[1]) * float(row[2])
             total_price += sum_price
     return total_price
 
-print(sys.argv)
 
-for index, a in enumerate(sys.argv):
-    print(f"argv{index} :: ", a)
-
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-
-else:
-    filename = manualFilename
-
-print("Total Cost :::", portfolio_cost(filename))
+print("Total Cost :::", portfolio_cost(""))
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,40 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+from pprint import pprint  # Pretty Print 모듈 임포트
+
+filename = (
+    "C:/Users/MAINUSER/Desktop/교육/Python/practical-python/Work/Data/portfolio.csv"
+)
+
+
+# def portfolio_cost(filename):
+#     portfolio = []
+#     total_cost = 0
+#     with open(f"{filename}", "rt") as f:
+#         rows = csv.reader(f)
+#         header = next(rows)  # 첫 번째 행은 헤더이므로 건너뜀
+
+#         for row in rows:
+#             portfolio.append(
+#                 {
+#                     "name": row[0],
+#                     "shares": int(row[1]),
+#                     "price": float(row[2]),
+#                 }
+#             )
+#             total_cost += int(row[1]) * float(row[2])
+#     return portfolio
+
+
+def read_price(filename):
+    with open(f"{filename}", "rt") as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            print(row)
+
+
+read_price(filename)
