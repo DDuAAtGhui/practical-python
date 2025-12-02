8c483208fc5ca8eecdcbb714c321b8c169d5bc4c
mobase <fmofmo1@gmail.com>
2025-12-02 12:18:12 +0900

    add detect fault data functioning

--- Work/pcost.py
+++ Work/pcost.py
@@ -10,11 +10,14 @@ def portfolio_cost(filename):
     total_price = 0
     with open(f"{filename}", "rt") as f:
         rows = csv.reader(f)
-
-        for row in rows:
-            sum_price = int(row[1]) * float(row[2])
-            total_price += sum_price
+        header = next(rows)
+        for i, row in enumerate(rows):
+            try:
+                sum_price = int(row[1]) * float(row[2])
+                total_price += sum_price
+            except ValueError:
+                print(f"Row {i} couldn't convert : {row}")
     return total_price
 
 
-print("Total Cost :::", portfolio_cost(""))
+print("Total Cost :::", portfolio_cost("C:/Users/user/Desktop/Workplace/개인/py/practical-python/Work/Data/missing.csv"))
